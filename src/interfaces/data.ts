d3ea46fd539a4948c8cd17e111f4e8d7dfe9cdfe
Adrián Rodríguez <adrianrdguez87@gmail.com>
2023-02-28 11:11:11 -0500

    feat: add interface

---
+++ src/interfaces/data.ts
@@ -0,0 +1,26 @@
+export interface Image {
+  png: string;
+  webp: string;
+}
+
+export interface User {
+  image: Image;
+  username: string;
+}
+export interface Reply {
+  id: number;
+  content: string;
+  createdAt: string;
+  score: number;
+  user: User;
+  replyingTo: string;
+}
+
+export interface Comment {
+  id: number;
+  content: string;
+  createdAt: string;
+  score: number;
+  user: User;
+  replies?: Reply[];
+}
