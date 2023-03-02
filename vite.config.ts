00cbe9d87135a262ac36cf02bc6b2bf6982208fd
Adrián Rodríguez <adrianrdguez87@gmail.com>
2023-03-02 00:15:23 -0500

    fix to publish

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/Interactive-comments-section-Frontend-Mentor-Vuejs/',
   plugins: [vue()],
 })
